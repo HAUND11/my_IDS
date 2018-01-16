0d78858f46a64f099ead7f9a1caaac0a836a22aa
HAUND11 <stentretre@gmail.com>
2018-01-16 19:57:33 +0300

    end for date 16.01.18

---
+++ test_ServerClient/client.py
@@ -0,0 +1,52 @@
+from data_parser import *
+import socket
+import re
+import logging
+
+
+class SETTINGS_BOT():
+
+    def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+        server_ip, host_ip,host_mask =SETTINGS_BOT.get_config_settings(self)
+        SETTINGS_BOT.connect_to_server(self,server_ip,host_ip)
+
+    def get_config_settings(self):
+        try:
+            config_file = open("bot_net_config.conf",'r')
+            settings_bot = config_file.read()
+            server_ip = re.findall(r'ip_server-(.*)',settings_bot)
+            host_ip = re.findall(r'ip_host-(.*)',settings_bot)
+            host_mask = re.findall(r'host_mask-(.*)',settings_bot)
+        except IOError:
+            logging.error("Error open config file")
+        finally:
+            config_file.close()
+
+        return server_ip,host_ip,host_mask
+
+    def connect_to_server(self,server_ip,host_ip):
+        bot_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        bot_socket.connect((server_ip[0],9000))
+        SYN_data = bytes(server_ip[0]+":CONNECT:SYN",encoding='utf-8')
+        bot_socket.sendall(SYN_data)
+        ACK_data = bot_socket.recv(1024)
+        if ACK_data == bytes(server_ip[0]+":CONNECT:ACK",encoding='utf-8'):
+            logging.info("BOT START %r",bot_socket)
+            logging.info("STARTING SNIFF NETWORK")
+            SNIFFER(bot_socket)
+
+class SNIFFER(object):
+
+    def __init__(self, bot_socket):
+        sniff_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
+        while True:
+            data, address = sniff_socket.recvfrom(65535)
+            header_packet = PARSER.start_parser(data)
+            print(header_packet)
+            if header_packet["3L"]["Protocol_type"] == 1:
+                bot_socket.sendall(data)
+
+
+if __name__ == "__main__":
+    bot = SETTINGS_BOT()
---
+++ test_ServerClient/multy_server.py
@@ -0,0 +1,65 @@
+import multiprocessing
+import socket
+server_name = "127.0.0.1"
+
+def handle(connection, address):                                                ### РАБОТА СЕРВЕРА С КЛИЕНТОМ
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("process-%r" % (address,))
+    try:
+        logger.debug("Connected %r at %r", connection, address)
+        while True:
+            SYN_data = connection.recv(1024)
+            if SYN_data == bytes(server_name+":CONNECT:SYN",encoding='utf-8'):
+                logger.debug("Received start work %r", SYN_data)
+                connection.sendall(bytes(address[0]+":CONNECT:ACK",encoding='utf-8'))
+                logger.debug("Sent data")
+                while True:
+                    packet_headers = connection.recv(1024)
+                    if packet_headers != b'':
+                        print(packet_headers)
+    except:
+        logger.exception("Problem handling request")
+    finally:
+        logger.debug("Closing socket")
+        connection.close()
+
+
+class Server(object):
+    def __init__(self, hostname, port):
+        import logging
+        self.logger = logging.getLogger("server")
+        self.hostname = hostname
+        self.port = port
+
+    def start(self):                                                            ### НАСТРОЙКА АДАПТЕРА И ПРИВЯЗКА
+        self.logger.debug("listening")
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.bind((self.hostname, self.port))
+        self.socket.listen(1)
+
+        while True:                                                             ### ОЖИДАНИЕ ПОДКЛЮЧЕНИЕ И ЗАПУСК ОТДЕЛЬНОГО ПРОЦЕССА ПОД КЛИЕНТА
+            conn, address = self.socket.accept()
+            self.logger.debug("Got connection")
+            process = multiprocessing.Process(target=handle, args=(conn, address))
+            process.daemon = True
+            process.start()
+            self.logger.debug("Started process %r", process)
+
+
+if __name__ == "__main__":
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    server = Server(server_name, 9000)                                            ### ИНИЦИАЛИАЦИЯ СЕРВЕРА class Server
+    try:
+        logging.info("Listening")
+        server.start()                                                           ### ЗАПУСК СЕРВЕРА
+    except:
+        logging.exception("Unexpected exception")
+    finally:
+        logging.info("Shutting down")
+        for process in multiprocessing.active_children():                        ### ЗАКРЫТИЕ ВСЕХ СОЕДИНЕНИЙ
+            logging.info("Shutting down process %r", process)
+            process.terminate()
+            process.join()
+logging.info("All done")
