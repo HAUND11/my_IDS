c63d3e34ff896ce05cd54230c39b661ae4cb6de7
HAUND11 <stentretre@gmail.com>
2018-01-24 17:18:39 +0300

    24.01.18(2 )

--- arp_scan.py
+++ arp_scan.py
@@ -1,13 +1,21 @@
 from scapy.all import *
 import sys
 import re
+import logging
 import threading
 import multiprocessing
 
 
 def save_arp(packet):
+	mac_ip_address_table = {}
 	if packet[ARP].op == 2: #response
-		return 'Response: {} has address {}'.format(packet[ARP].hwsrc, packet[ARP].psrc)
+		check_ip_mac = False
+		if packet[ARP].psrc in mac_ip_address_table:
+				check_ip_mac = True
+		if check_ip_mac == False:
+			mac_ip_address_table[packet[ARP].psrc] = packet[ARP].hwsrc
+		return logging.info('Response: {} has address {}'.format(packet[ARP].hwsrc, packet[ARP].psrc))
+
 def sniff_arp():
 	sniff(prn=save_arp, filter='arp')
 
@@ -19,19 +27,19 @@ def scan_arp(network_ip,mask):
 	for mask_id in mask:
 		if mask_id != '255':
 			if for_id == 0:
-				print("0")
+				logging.error("ERROR MASK")
 				break
 			elif for_id == 1:
-				print("1")
+				logging.error("ERROR MASK")
 				break
 			elif for_id == 2:
-				for id_ip_2 in range(network_ip[2],255):
-					for id_ip_3 in range(network_ip[3], 255):
+				for id_ip_2 in range(network_ip[2],254):
+					for id_ip_3 in range(network_ip[3], 254):
 						scan_ip = str(network_ip[0]) + "." + str(network_ip[1]) + "." + str(id_ip_2) + "." + str(id_ip_3)
 						start_scan(scan_ip)
 				break
 			elif for_id == 3:
-				for id_ip in range(network_ip[3],255):
+				for id_ip in range(network_ip[3],254):
 					scan_ip = str(network_ip[0])+"."+str(network_ip[1])+"."+str(network_ip[2])+"."+str(id_ip)
 					start_scan(scan_ip)
 				break
@@ -44,7 +52,6 @@ def start_scan(host_ip):
 
 if __name__ == "__main__":
 	try:
-		import logging
 		logging.basicConfig(level=logging.DEBUG)
 		network_ip = re.findall(r'(\d+).',sys.argv[1]+'.')
 		network_mask = re.findall(r'(\d+).',sys.argv[2]+'.')
@@ -55,5 +62,5 @@ if __name__ == "__main__":
 		start_sniff.start()
 		scan_arp(main_network_ip,network_mask)
 	except:
-		print("#### Write ip interface and mask network ####")
-		print("#### Example /python3 {fale_name} 192.168.2.0 255.255.255.0 ####")
+		logging.info("#### Write ip interface and mask network ####")
+		logging.info("#### Example /python3 {fale_name} 192.168.2.0 255.255.255.0 ####")
